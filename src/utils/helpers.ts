f9192375cd621d6cfa01ffa511f869122fbde7ea
Muhammad-Abdullah012 <not.muhammad.abdullah68@gmail.com>
2024-07-11 19:17:07 +0500

    perf: use pg instead of sequelize and cache query result using redis

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,11 +1,19 @@
+import Container from 'typedi';
 import { pgPool } from '@/config/sequelize';
+import { RedisService } from '@/services/redis.service';
 
 export const getPropertyTypes = async () => {
   return (await pgPool.query('SELECT DISTINCT type FROM property_v2;')).rows.map(item => item?.['type']).filter(item => item != null);
 };
 
 export const getPropertyPurpose = async () => {
-  return (await pgPool.query<{ purpose: string }>('SELECT DISTINCT purpose FROM property_v2;')).rows
-    .map(item => item?.['purpose'])
-    .filter(item => item != null);
+  const redis = Container.get(RedisService);
+  const query = 'SELECT DISTINCT purpose FROM property_v2;';
+  const redisValue = await redis.getRedisValue(query);
+  if (redisValue) {
+    return JSON.parse(redisValue);
+  }
+  const propertyPurpose = (await pgPool.query<{ purpose: string }>(query)).rows.map(item => item?.['purpose']).filter(item => item != null);
+  await redis.setRedisValue({ key: query, value: JSON.stringify(propertyPurpose) });
+  return propertyPurpose;
 };
