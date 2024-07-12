4d42323e07b8a5f6e1bb1f1f7e8e3f04efa1e21a
Muhammad-Abdullah012 <not.muhammad.abdullah68@gmail.com>
2024-07-12 14:44:12 +0500

    refactor: cache property types

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -3,7 +3,15 @@ import { pgPool } from '@/config/sequelize';
 import { RedisService } from '@/services/redis.service';
 
 export const getPropertyTypes = async () => {
-  return (await pgPool.query('SELECT DISTINCT type FROM property_v2;')).rows.map(item => item?.['type']).filter(item => item != null);
+  const redis = Container.get(RedisService);
+  const query = 'SELECT DISTINCT type FROM property_v2;';
+  const redisValue = await redis.getRedisValue(query);
+  if (redisValue) {
+    return JSON.parse(redisValue);
+  }
+  const propertyTypes = (await pgPool.query(query)).rows.map(item => item?.['type']).filter(item => item != null);
+  redis.setRedisValue({ key: query, value: JSON.stringify(propertyTypes) });
+  return propertyTypes;
 };
 
 export const getPropertyPurpose = async () => {
@@ -14,6 +22,6 @@ export const getPropertyPurpose = async () => {
     return JSON.parse(redisValue);
   }
   const propertyPurpose = (await pgPool.query<{ purpose: string }>(query)).rows.map(item => item?.['purpose']).filter(item => item != null);
-  await redis.setRedisValue({ key: query, value: JSON.stringify(propertyPurpose) });
+  redis.setRedisValue({ key: query, value: JSON.stringify(propertyPurpose) });
   return propertyPurpose;
 };
